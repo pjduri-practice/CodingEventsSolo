d74604a7e2a25462b7d9b5c662adfbbb4f828082
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-26 18:03:44 -0500

    added form to add event with corresponding action methods in controller

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        private static List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+            return View();
+        }
+
+        // GET: /<controller>/add
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View();
+        }
+
+        // POST: /<controller>/add
+        [HttpPost("events/add")]
+        public IActionResult NewEvent(string name) 
+        {
+            Events.Add(name);
+            return Redirect("/events");
+        }
+    }
+}
