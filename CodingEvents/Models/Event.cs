3b13fc6bc5c904e9fb14ee8ee18b931b69016029
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-26 21:46:48 -0500

    added Event class and updated EventsController to use it

---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Event(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+
+    }
+}
