461b5cfa4711ddf6f47d7c3044f38aa7d93d7bc4
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-29 00:20:10 -0500

    added validation attributes and contact email property to view model

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -1,9 +1,20 @@
-﻿namespace CodingEvents.ViewModels
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
 {
     public class AddEventViewModel
     {
-
+        [Required(ErrorMessage = "Name of event is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Event name should be between 3 and 50 characters long.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Event description is required.")]
+        [StringLength(500, ErrorMessage = "Description is over 500 characters.")]
         public string? Description { get; set; }
+        //
+        [EmailAddress]
+        public string? ContactEmail { get; set; }
+
     }
 }
